0365b05decc5b39d607a7345557d3a3e28f39d1c
Nehal <nehal.mohanlal12@gmail.com>
2024-08-15 18:47:50 +0200

    new prompt

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+# Specific Model
 model_name = "facebook/blenderbot-400M-distill" 
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+#Save Convo History for more learning
 conversation_histrory = [] 
 
 history_string = "\n".join(conversation_histrory) 
 
-input_text = "how many countries are there in the world?" 
-
+#prompt
+input_text = "Tell me a Joke?" 
 inputs = tokenizer(history_string + input_text, return_tensors="pt")
 
 # Generate the response
